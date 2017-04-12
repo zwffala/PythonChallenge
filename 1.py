b28a9f0f017d91745f58b0d8213b3f6157f9eb2a
fala <fala>
2017-04-12 20:30:17 +0200

    add solution for level1

---
+++ 1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+input = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+
+intab = "abcdefghijklmnopqrstuvwxyz"
+outtab = "cdefghijklmnopqrstuvwxyzab"
+trantab = str.maketrans(intab, outtab)
+print(input.translate(trantab))
+
+url = "map"
+print(url.translate(trantab))
